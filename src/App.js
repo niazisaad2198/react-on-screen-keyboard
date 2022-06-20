2e792ae0b650cd1fcf82c15cc8eefde1333bd31d
Muhammad Saad Khan Niazi <niazisaad2198@gmail.com>
2022-06-20 19:21:21 +0500

    Added handling for CapsLock and Shift keys.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,73 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import KeyboardButton from "./components/KeyboardButton";
 import { useSelector, useDispatch } from "react-redux";
+import { toggleCapsLock, setCapsLock, } from "./slices/KeyboardSlice";
 
 const App = () => {
   const input = useSelector((state) => state.kbState.text);
+  let modifierPressed=false
   const dispatch = useDispatch();
 
   useEffect(() => {
-    document.addEventListener("keydown", (e)=>addListener(e));
-    return document.removeEventListener("keydown",(e)=>addListener(e))
+    addEventListeners();
+    return removeEventListeners()
   }, []);
 
-  const addListener = (e) => {
+  const handleKeyDown = (e) => {
     const value = e.key;
-    console.log(value);
-    //dispatch
+    console.log(value + ' down');
+    if(!modifierPressed){
+      switch (value) {
+        case 'Shift':
+          modifierPressed=true
+          dispatch(setCapsLock(true))
+          break;
+        case 'CapsLock':
+          modifierPressed=true
+          break;
+      
+        default:
+          break;
+      }
+    }
+  }
+  const handleKeyUp = (e) => {
+    const value = e.key;
+    console.log(value + ' up');
+    if(modifierPressed){
+      switch (value) {
+        case 'Shift':
+          modifierPressed=false
+          dispatch(setCapsLock(false))
+          break;
+        
+        case 'CapsLock':
+          modifierPressed=false
+          dispatch(toggleCapsLock())
+          break;
+      
+        default:
+          break;
+      }
+    }
+  }
+  const checkCapsLock = ()=>{
+    const event = document.createEvent('KeyboardEvent')
+    console.log(event);
+    const isCapsLockOn = event.getModifierState('CapsLock')
+    if(isCapsLockOn)dispatch(toggleCapsLock());console.log("Caps Lock ON");
+    return
+  }
+
+
+  const addEventListeners =()=>{
+    document.addEventListener("keydown", (e)=>handleKeyDown(e));
+    document.addEventListener("keyup", (e)=>handleKeyUp(e));
+  }
+
+  const removeEventListeners =()=>{
+    document.removeEventListener("keydown", (e)=>handleKeyDown(e));
+    document.removeEventListener("keyup", (e)=>handleKeyUp(e));
   }
 
   return (
--- src/components/KeyboardButton.js
+++ src/components/KeyboardButton.js
@@ -1,7 +1,8 @@
+import { useEffect } from "react";
 import Button from "react-bootstrap/Button";
-import Alert from "react-bootstrap/Alert";
 import { useSelector, useDispatch } from "react-redux";
 import { updateText } from "../slices/KeyboardSlice";
+
 
 export default function KeyboardButton({ text }) {
   const isCapsLockOn = useSelector((state) => state.kbState.isCapsLockOn);
@@ -12,10 +13,8 @@ export default function KeyboardButton({ text }) {
   };
 
   return (
-    <div>
-      <Button onClick={handleClick}>
-        {isCapsLockOn ? text : text.toLowerCase()}
-      </Button>
-    </div>
+    <Button onClick={handleClick}>
+      {isCapsLockOn? text : text.toLowerCase()}
+    </Button>
   );
 }
