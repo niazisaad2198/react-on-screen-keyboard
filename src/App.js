ce5cec297ccf1e332b0f865d02122e7785516984
Muhammad Saad Khan Niazi <niazisaad2198@gmail.com>
2022-06-20 19:57:23 +0500

    Added TextArea. (WIP)

--- src/App.js
+++ src/App.js
@@ -1,80 +1,99 @@
 import { useState, useEffect } from "react";
+
+// React Bootstrap component imports
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+
+// Custom component imports
 import KeyboardButton from "./components/KeyboardButton";
+
+// Redux imports
 import { useSelector, useDispatch } from "react-redux";
-import { toggleCapsLock, setCapsLock, } from "./slices/KeyboardSlice";
+import { toggleCapsLock, setCapsLock } from "./slices/KeyboardSlice";
+import { FormLabel } from "react-bootstrap";
 
 const App = () => {
   const input = useSelector((state) => state.kbState.text);
-  let modifierPressed=false
+  let modifierPressed = false;
   const dispatch = useDispatch();
 
   useEffect(() => {
     addEventListeners();
-    return removeEventListeners()
+    return removeEventListeners();
   }, []);
 
   const handleKeyDown = (e) => {
     const value = e.key;
-    console.log(value + ' down');
-    if(!modifierPressed){
+    console.log(value + " down");
+    if (!modifierPressed) {
       switch (value) {
-        case 'Shift':
-          modifierPressed=true
-          dispatch(setCapsLock(true))
+        case "Shift":
+          modifierPressed = true;
+          dispatch(setCapsLock(true));
           break;
-        case 'CapsLock':
-          modifierPressed=true
+        case "CapsLock":
+          modifierPressed = true;
           break;
-      
+
         default:
           break;
       }
     }
-  }
+  };
   const handleKeyUp = (e) => {
     const value = e.key;
-    console.log(value + ' up');
-    if(modifierPressed){
+    console.log(value + " up");
+    if (modifierPressed) {
       switch (value) {
-        case 'Shift':
-          modifierPressed=false
-          dispatch(setCapsLock(false))
+        case "Shift":
+          modifierPressed = false;
+          dispatch(setCapsLock(false));
           break;
-        
-        case 'CapsLock':
-          modifierPressed=false
-          dispatch(toggleCapsLock())
+
+        case "CapsLock":
+          modifierPressed = false;
+          dispatch(toggleCapsLock());
           break;
-      
+
         default:
           break;
       }
     }
-  }
-  const checkCapsLock = ()=>{
-    const event = document.createEvent('KeyboardEvent')
+  };
+  const checkCapsLock = () => {
+    const event = document.createEvent("KeyboardEvent");
     console.log(event);
-    const isCapsLockOn = event.getModifierState('CapsLock')
-    if(isCapsLockOn)dispatch(toggleCapsLock());console.log("Caps Lock ON");
-    return
-  }
+    const isCapsLockOn = event.getModifierState("CapsLock");
+    if (isCapsLockOn) dispatch(toggleCapsLock());
+    console.log("Caps Lock ON");
+    return;
+  };
 
+  const addEventListeners = () => {
+    document.addEventListener("keydown", (e) => handleKeyDown(e));
+    document.addEventListener("keyup", (e) => handleKeyUp(e));
+  };
 
-  const addEventListeners =()=>{
-    document.addEventListener("keydown", (e)=>handleKeyDown(e));
-    document.addEventListener("keyup", (e)=>handleKeyUp(e));
-  }
+  const removeEventListeners = () => {
+    document.removeEventListener("keydown", (e) => handleKeyDown(e));
+    document.removeEventListener("keyup", (e) => handleKeyUp(e));
+  };
 
-  const removeEventListeners =()=>{
-    document.removeEventListener("keydown", (e)=>handleKeyDown(e));
-    document.removeEventListener("keyup", (e)=>handleKeyUp(e));
-  }
+  /*
+  1. Add text Field
+  2. Add keyboard component 
+  
+  */
 
   return (
-    <div>
-      <h1>Text: {input}</h1>
+    <Container>
+      <Form>
+        <Form.Group>
+          <Form.Control size="lg" style={{backgroundColor:'white'}} readOnly placeholder="Text" as='textarea' value={input} rows={3}/>
+        </Form.Group>
+      </Form>
       <KeyboardButton text="A" />
-    </div>
+    </Container>
   );
 };
 
